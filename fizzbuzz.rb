c9bcb75a9310d1ac83b1f7ed0860c972ad408ee0
Halie Hall <halierae17@outlook.com>
2021-03-26 16:10:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ end
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
+  elsif int % 5 == 0 
+  "Buzz"
+  elsif int % 3 && 5 == 0 
+  "FizzBuzz"
   end 
 end
-def fizzbuzz(int)
-  if int % 5 == 0
-    "Buzz"
-  end
-end
+
